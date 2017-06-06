24cca37f16a0cee5c3568be2e66ad8c08892cbce
hilla <hilla.guz@gmail.com>
2017-06-06 07:52:20 +0300

    start adding classes

---
+++ B17 Ex03/Ex03.GarageLogic/Vehicle.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Collections.Generic;
+
+namespace Ex03.GarageLogic
+{
+    abstract class Vehicle
+    {
+        string m_ModelName;
+        string m_LicenceNumber;
+        float m_CurrentEnergyLevel;
+        List<Weel> m_Weels;
+    }
+}
---
+++ B17 Ex03/Ex03.GarageLogic/Weel.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Ex03.GarageLogic
+{
+    class Weel
+    {
+        string m_ManufacturerName;
+        float m_CurrentAirPressure;
+        float m_MaxAirPressure;
+
+        public void FillAir(float i_AirToAdd)
+        {
+            // מקבלת כמה אוויר להוסיף לגלגל, משנה לחץ אוויר אם הוא לא חורג מהמקס
+        }
+    }
+}
