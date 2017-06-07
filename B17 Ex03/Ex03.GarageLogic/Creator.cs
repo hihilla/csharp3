52c8923bc3e4d7c93ec35af8a1731c3cf5af46dc
hilla <hilla.guz@gmail.com>
2017-06-07 12:21:37 +0300

    duplicated code

--- B17 Ex03/Ex03.GarageLogic/Creator.cs
+++ B17 Ex03/Ex03.GarageLogic/Creator.cs
@@ -5,70 +5,40 @@ namespace Ex03.GarageLogic
     {
         Garage m_Garage = new Garage();
 
-        public Motorcycle CreateNewRegularMotorcycle(string i_ModelName, string i_LicenceNumber, 
-                                                    float i_CurrentEnergyLevel, string i_OwnerName, 
-                                                    string i_OwnerPhoneNumber, string i_ManufacturerName,
-                                                    Motorcycle.e_LicenceType i_LicenceType, int i_EngineCapacity)
+        public Motorcycle CreateNewMotorcycle(string i_ModelName, string i_LicenceNumber,
+                                                float i_CurrentEnergyLevel, string i_OwnerName,
+                                                string i_OwnerPhoneNumber, string i_ManufacturerName,
+                                                Motorcycle.e_LicenceType i_LicenceType, int i_EngineCapacity,
+                                                bool i_IsElectric)
         {
             int numberOfWheels = 2;
             float maxWheelAirPressure = 33;
-            Vehicle.e_EnergyType energyType = Vehicle.e_EnergyType.Fuel;
-            Vehicle.e_FuelType fuelType = Vehicle.e_FuelType.Octan95;
-            float tankSize = 5.5F;
+            Vehicle.e_EnergyType energyType = i_IsElectric ? Vehicle.e_EnergyType.Electric : Vehicle.e_EnergyType.Fuel;
+            Vehicle.e_FuelType fuelType = i_IsElectric ? Vehicle.e_FuelType.none : Vehicle.e_FuelType.Octan95;
+            float tankSize = i_IsElectric ? 2.7F : 5.5F;
             return new Motorcycle(i_ModelName, i_LicenceNumber, energyType, fuelType,
                           i_CurrentEnergyLevel, tankSize, i_OwnerName, i_OwnerPhoneNumber,
                           i_ManufacturerName, maxWheelAirPressure, numberOfWheels, i_LicenceType,
                           i_EngineCapacity);
         }
 
-        public Motorcycle CreateNewElectricMotorcycle(string i_ModelName, string i_LicenceNumber,
-                                                    float i_CurrentEnergyLevel, string i_OwnerName,
-                                                    string i_OwnerPhoneNumber, string i_ManufacturerName,
-                                                    Motorcycle.e_LicenceType i_LicenceType, int i_EngineCapacity)
-        {
-            int numberOfWheels = 2;
-            float maxWheelAirPressure = 33;
-            Vehicle.e_EnergyType energyType = Vehicle.e_EnergyType.Electric;
-            Vehicle.e_FuelType fuelType = Vehicle.e_FuelType.none;
-            float batteryTime = 2.7F;
-            return new Motorcycle(i_ModelName, i_LicenceNumber, energyType, fuelType,
-                          i_CurrentEnergyLevel, batteryTime, i_OwnerName, i_OwnerPhoneNumber,
-                          i_ManufacturerName, maxWheelAirPressure, numberOfWheels, i_LicenceType,
-                          i_EngineCapacity);
-        }
-
-        public Car CreateNewRegularCar(string i_ModelName, string i_LicenceNumber, 
-                                        float i_CurrentEnergyLevel, string i_OwnerName, 
-                                        string i_OwnerPhoneNumber, string i_ManufacturerName,
-                                        Car.e_Color i_CarColor, int i_NumOfDoors)
-        {
-            int numberOfWheels = 4;
-            float maxWheelAirPressure = 30;
-            Vehicle.e_EnergyType energyType = Vehicle.e_EnergyType.Fuel;
-            Vehicle.e_FuelType fuelType = Vehicle.e_FuelType.Octan98;
-            float tankSize = 42F;
-            return new Car(i_ModelName, i_LicenceNumber, energyType, fuelType,
-                    i_CurrentEnergyLevel, tankSize, i_OwnerName, i_OwnerPhoneNumber,
-                    i_ManufacturerName, maxWheelAirPressure, numberOfWheels, i_CarColor, i_NumOfDoors);
-        }
-
-        public Car CreateNewElectricCar(string i_ModelName, string i_LicenceNumber,
+        public Car CreateNewCar(string i_ModelName, string i_LicenceNumber,
                                         float i_CurrentEnergyLevel, string i_OwnerName,
                                         string i_OwnerPhoneNumber, string i_ManufacturerName,
-                                        Car.e_Color i_CarColor, int i_NumOfDoors)
+                                        Car.e_Color i_CarColor, int i_NumOfDoors, bool i_IsElectric)
         {
             int numberOfWheels = 4;
             float maxWheelAirPressure = 30;
-            Vehicle.e_EnergyType energyType = Vehicle.e_EnergyType.Electric;
-            Vehicle.e_FuelType fuelType = Vehicle.e_FuelType.none;
-            float batteryTime = 2.5F;
+            Vehicle.e_EnergyType energyType = i_IsElectric ? Vehicle.e_EnergyType.Electric : Vehicle.e_EnergyType.Fuel;
+            Vehicle.e_FuelType fuelType = i_IsElectric ? Vehicle.e_FuelType.none : Vehicle.e_FuelType.Octan98;
+            float maxEnergyLevel = i_IsElectric ? 2.5F : 42F;
             return new Car(i_ModelName, i_LicenceNumber, energyType, fuelType,
-                    i_CurrentEnergyLevel, batteryTime, i_OwnerName, i_OwnerPhoneNumber,
+                    i_CurrentEnergyLevel, maxEnergyLevel, i_OwnerName, i_OwnerPhoneNumber,
                     i_ManufacturerName, maxWheelAirPressure, numberOfWheels, i_CarColor, i_NumOfDoors);
         }
 
         public Truck CreateNewTruck(string i_ModelName, string i_LicenceNumber,
-                                    float i_CurrentEnergyLevel, string i_OwnerName, 
+                                    float i_CurrentEnergyLevel, string i_OwnerName,
                                     string i_OwnerPhoneNumber, string i_ManufacturerName,
                                     bool i_IsCarryingHazardousMaterials, float i_MaxCarryingWeight)
         {
@@ -77,7 +47,7 @@ namespace Ex03.GarageLogic
             Vehicle.e_EnergyType energyType = Vehicle.e_EnergyType.Fuel;
             Vehicle.e_FuelType fuelType = Vehicle.e_FuelType.Octan96;
             float tankSize = 135F;
-            return new Truck(i_ModelName, i_LicenceNumber, energyType,fuelType,
+            return new Truck(i_ModelName, i_LicenceNumber, energyType, fuelType,
                     i_CurrentEnergyLevel, tankSize, i_OwnerName, i_OwnerPhoneNumber,
                     i_ManufacturerName, maxWheelAirPressure, numberOfWheels, i_IsCarryingHazardousMaterials,
                     i_MaxCarryingWeight);
