0f4f891f025a2444ca16487682cd8bb11dca8f98
hilla <hilla.guz@gmail.com>
2017-06-11 09:34:53 +0300

    validation of action, license numbers and change state

--- B17 Ex03/Ex03.ConsoleUI/Program.cs
+++ B17 Ex03/Ex03.ConsoleUI/Program.cs
@@ -24,7 +24,7 @@ namespace Ex03.ConsoleUI
             Console.WriteLine("Press 7 to display full details of a vehicle.");
             string userAnswer = Console.ReadLine();
             int chosenAction;
-            while (!int.TryParse(userAnswer, out chosenAction)) 
+            while (!int.TryParse(userAnswer, out chosenAction) || !(chosenAction >= 1 && chosenAction <= 7)) 
             {
                 Console.WriteLine("Invalid action. please choose valid action.");
                 userAnswer = Console.ReadLine();
@@ -63,12 +63,56 @@ namespace Ex03.ConsoleUI
 
         private static void printLicenceNumbersOfVehicles(GarageLogic.Garage i_Garage)
         {
+            Console.WriteLine("Do you want to print with filter?");
+            Console.WriteLine("Press 0 for no filter, 1 for Repair In Progress, 2 for Repair Complete, 3 for Paid");
+            string userAnswer = Console.ReadLine();
+            int chosenFilter;
+            while (!int.TryParse(userAnswer, out chosenFilter) || !(chosenFilter >= 0 && chosenFilter <= 3))
+            {
+                Console.WriteLine("Invalid filter. please choose valid filter.");
+            }
 
+            switch (chosenFilter)
+            {
+                case 0:
+                    i_Garage.GetLicenceNumbers();
+                    break;
+                case 1:
+                    i_Garage.GetLicenceNumbers(GarageLogic.Vehicle.e_VehicleState.RepairInProgress);
+                    break;
+                case 2:
+                    i_Garage.GetLicenceNumbers(GarageLogic.Vehicle.e_VehicleState.RepairComplete);
+                    break;
+                case 3:
+                    i_Garage.GetLicenceNumbers(GarageLogic.Vehicle.e_VehicleState.Paid);
+                    break;
+            }
         }
 
         private static void changeState(GarageLogic.Garage i_Garage)
         {
-
+            Console.WriteLine("Please enter the vehicle licence number");
+            string licenceNumber = Console.ReadLine();
+            Console.WriteLine("Please enter 1 for Repair In Progress, 2 for Repair Complete, 3 for Paid.");
+            int chosenState;
+            while (!int.TryParse(Console.ReadLine(), out chosenState) || !(chosenState >= 1 && chosenState <= 3))
+            {
+                Console.WriteLine("Invalid state. please choose valid state.");
+            }
+            GarageLogic.Vehicle.e_VehicleState vehicleState = GarageLogic.Vehicle.e_VehicleState.RepairInProgress;
+            switch (chosenState)
+            {
+                case 1:
+                    vehicleState = GarageLogic.Vehicle.e_VehicleState.RepairInProgress;
+                    break;
+                case 2:
+                    vehicleState = GarageLogic.Vehicle.e_VehicleState.RepairComplete;
+                    break;
+                case 3:
+                    vehicleState = GarageLogic.Vehicle.e_VehicleState.Paid;
+                    break;
+            }
+            i_Garage.ChangeVehicleState(licenceNumber, vehicleState);
         }
 
         private static void fillAirInVehicle(GarageLogic.Garage i_Garage)
