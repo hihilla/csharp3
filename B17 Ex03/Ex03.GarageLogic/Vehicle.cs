65d147a343eb375588e2075b4676291c21086b8a
Adar <adarlavi@gmail.com>
2017-06-13 20:16:29 +0300

    stiupid correction of exception

--- B17 Ex03/Ex03.GarageLogic/Vehicle.cs
+++ B17 Ex03/Ex03.GarageLogic/Vehicle.cs
@@ -140,7 +140,7 @@ namespace Ex03.GarageLogic
 
             if (manufacturers.Length != this.m_Wheels.Count)
             {
-                string exceptionMsg = string.Format("Not Enough arguments. There are {0} wheels, and {1} arguments",
+                string exceptionMsg = string.Format("Number of arguments is not compatible. There are {0} wheels, and {1} arguments",
                                                     this.m_Wheels.Count, manufacturers.Length);
                 throw new ArgumentException(exceptionMsg);
             }
